9abdd535ecf12b779f08693f47149a374546b404
Aleksandr Viznyi <senak98@yandex.ru>
2025-09-27 18:06:44 +0300

    gg

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,17 +1,20 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { todoListApi } from "./api";
 import { useState } from "react";
 
 export function TodoList() {
   const [page, setPage] = useState(1);
-  const { data: todoItems, error, isPending } = useQuery({
+  const [enabled, setEnabled] = useState(false)
+  const { data: todoItems, error, isPending, isFetching, isLoading, status, fetchStatus, isPlaceholderData } = useQuery({
     queryKey: ["tasks", "list", { page }],
-    queryFn: meta => todoListApi.getTodoList({ page }, meta)
+    queryFn: meta => todoListApi.getTodoList({ page }, meta),
+    placeholderData: keepPreviousData,
+    enabled: enabled
   });
 
-  console.log(page);
+  console.log({status, fetchStatus});
 
-  if (isPending) {
+  if (isLoading) {
     return <div>Loading</div>;
   }
 
@@ -22,13 +25,16 @@ export function TodoList() {
   return (
     <div className="p-5 mx-auto max-w-[1200px] mt-10">
       <h1 className="text-3xl font-bold underline mb-5">Todo list</h1>
-      <div className="flex flex-col gap-4">
-        {todoItems.data.map(el => (
+      <div>Page {page} from {todoItems?.pages}</div>
+      <button onClick={() => setEnabled(prev => !prev)}>Toggle enabled</button>
+      <div className={"flex flex-col gap-4" + (isPlaceholderData ? ' opacity-50' : '')}>
+        {todoItems?.data.map(el => (
           <div className="border border-slate-300 rounded p-3" key={el.id}>
             {el.text}
           </div>
         ))}
       </div>
+
       <div className="flex gap-2 mt-4">
         <button
           onClick={() => setPage(p => Math.max(p - 1, 1))}
@@ -37,7 +43,7 @@ export function TodoList() {
           prev
         </button>
         <button
-          onClick={() => setPage(p => Math.min(p + 1, todoItems.pages))}
+          onClick={() => setPage(p => Math.min(p + 1, todoItems?.pages ?? 1))}
           className="p-3 border rounded border-teal-500"
         >
           next
